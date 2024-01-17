64e9b9c270fa5ed7c5b8b6f86bbfa6cbfcef33c7
fless-lab <achilleatarmla@gmail.com>
2024-01-17 12:04:17 +0000

    User model enhanced

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['customer', 'driver', 'admin'], required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["customer", "driver", "admin"],
+      default: "customer",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
+userSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.methods.updatePassword = async function (newPassword) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    this.password = hashedPassword;
+    await this.save();
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
