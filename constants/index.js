aa2acda9d544cacc898b5330402cec0fde4414a6
fless-lab <achilleatarmla@gmail.com>
2024-01-19 08:58:01 +0000

    Constants added to enhance readability

---
+++ constants/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+    EVENT_TYPES:{
+        
+    },
+    DELIVERY_STATUS:{
+        OPEN:'open', 
+        PICKED_UP:'picked-up', 
+        IN_TRANSIT:'in-transit', 
+        DELIVERED:'delivered', 
+        FAILED:'failed'
+    },
+    USER_TYPES:{
+        CUSTOMER:"customer", 
+        DRIVER:"driver", 
+        ADMIN:"admin"
+    }
+
+}
